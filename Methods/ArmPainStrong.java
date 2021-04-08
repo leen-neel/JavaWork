9208f6244c448ca34ea1e7f4f83cf7d6954b689f
AndyTheGameDev <and24903@gmail.com>
2021-04-08 13:57:25 +0530

    number number

---
+++ Methods/ArmPainStrong.java
@@ -0,0 +1,34 @@
+package Methods;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArmPainStrong {
+
+    public static boolean isArmStrong(int n) {
+
+        String numStr = Integer.toString(n);
+        int sum = 0;
+
+        for (int i = 0; i < numStr.length(); i++) {
+            sum += Math.pow(Character.getNumericValue(numStr.charAt(i)), 3);
+        }
+
+        return sum == n;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a number : ");
+        int n = Integer.parseInt(in.readLine());
+
+        System.out.println(
+                isArmStrong(n) ? "The number is an Armstrong number" : "The number isn't an Armstrong number.");
+
+    }
+
+}
---
+++ Methods/NeonPain.java
@@ -0,0 +1,32 @@
+package Methods;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NeonPain {
+
+    public static boolean isNeon(int n) {
+
+        String squareStr = Integer.toString((int) Math.pow(n, 2));
+        int sumOfSquareDigits = 0;
+
+        for (int i = 0; i < squareStr.length(); i++) {
+            sumOfSquareDigits += Character.getNumericValue(squareStr.charAt(i));
+        }
+
+        return sumOfSquareDigits == n;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a number : ");
+        int n = Integer.parseInt(in.readLine());
+
+        System.out.println(isNeon(n) ? "The numer is Neon." : "The number isn't neon.");
+
+    }
+
+}
