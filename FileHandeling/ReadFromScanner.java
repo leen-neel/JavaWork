ca2eda939f4415fd160a0fc174dfe4569ac60579
AndyTheGameDev <and24903@gmail.com>
2021-04-08 20:13:51 +0530

    more file manipulate thing....

--- FileHandeling/ReadFromScanner.java
+++ FileHandeling/ReadFromScanner.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 
 public class ReadFromScanner {
 
+    public static boolean isVowel(char ch) {
+        ch = Character.toUpperCase(ch);
+        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+
+    public static String removeVowels(String str) {
+
+        String sanitized = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (isVowel(str.charAt(i))) {
+                sanitized += '#';
+            } else {
+                sanitized += str.charAt(i);
+            }
+        }
+
+        return sanitized;
+
+    }
+
     public static void main(String[] args) throws Exception {
 
         File file = new File("/home/and249/RandomJavaStuff/Help_Pls/FileHandeling/text.txt");
@@ -16,7 +37,7 @@ public class ReadFromScanner {
         str = sc.next();
         sc.close();
 
-        System.out.println(str);
+        System.out.println(removeVowels(str));
     }
 
 }
