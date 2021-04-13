a1bef1048207e5ed13947358671c9ba04d37a08c
AndyTheGameDev <and24903@gmail.com>
2021-04-13 19:42:38 +0530

    sum patterns aka pain

---
+++ Loops/StringPatterAkaPain.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class StringPatterAkaPain {
+
+    public static void main(String[] args) {
+
+        String str = "ICSEISPAIN";
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(str.charAt(j) + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
