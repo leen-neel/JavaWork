a96124ebcb26d74193200a1d1131d538b3979faa
AndyTheGameDev <and24903@gmail.com>
2021-05-23 16:12:41 +0530

    help Im dum

---
+++ Methods/WeirdMultiply.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class WeirdMultiply {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+        int c = 0;
+
+        for (int i = 1; i <= b; i++) {
+            c += a;
+        }
+
+        System.out.println(c);
+
+    }
+
+}
