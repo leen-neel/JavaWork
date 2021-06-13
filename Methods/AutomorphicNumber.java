59577a4ec1a2d42802ecdfb04b8426ca9df3cac9
AndyTheGameDev <and24903@gmail.com>
2021-06-13 18:44:01 +0530

    main first becos yes

---
+++ Methods/AutomorphicNumber.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AutomorphicNumber {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(Syste, in));
+
+        System.out.print("Enter a number : ");
+        int n = Integer.parseInt(in.readLine());
+
+    }
+
+}
