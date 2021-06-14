d875f728688096c29c0066708994fdf3c8bd1c1d
AndyTheGameDev <and24903@gmail.com>
2021-06-14 17:51:58 +0530

    main first becos yes

---
+++ Methods/SwitchCase.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SwitchCase {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a sentence : ");
+        String str = in.readLine();
+
+    }
+
+}
