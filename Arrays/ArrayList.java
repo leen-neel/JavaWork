a8322dae4b6c46b84ccb5005e4232d3de6815972
AndyTheGameDev <and24903@gmail.com>
2021-06-17 17:07:19 +0530

    idk

---
+++ Arrays/ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+    }
+}
