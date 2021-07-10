107919c848ed1259e7669aee9cab5e62757e0997
AndyTheGameDev <and24903@gmail.com>
2021-07-10 18:53:40 +0530

    idk

--- Strings/CountThe.java
+++ Strings/CountThe.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CountThe {
 
     public static int numberOfThe(String str) {
@@ -7,7 +11,7 @@ public class CountThe {
 
         for (int i = 0; i < words.length; i++) {
 
-            if (words[i].equals(" the ")) {
+            if (words[i].equals("the")) {
 
                 count += 1;
 
@@ -16,6 +20,19 @@ public class CountThe {
         }
 
         return count;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a sentence : ");
+        String str = in.readLine();
+
+        System.out.println();
+
+        System.out.println("The number of times \"the\" has been mentioned : " + numberOfThe(str));
 
     }
 
