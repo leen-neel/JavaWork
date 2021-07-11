2df2e1859417b650c6a9a190cf84052d9c8c921c
AndyTheGameDev <and24903@gmail.com>
2021-07-11 18:34:24 +0530

    now I can count vowel

--- Strings/CountVowel.java
+++ Strings/CountVowel.java
@@ -3,7 +3,21 @@ public class CountVowel {
     public static boolean isVowel(char ch) {
 
         ch = Character.toUpperCase(ch);
-        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || chat == 'U';
+        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+
+    }
+
+    public static int countVowel(String str) {
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (isVowel(str.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
 
     }
 
