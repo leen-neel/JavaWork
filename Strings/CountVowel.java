9289db14f81bd4942e87b816ed8d230ac30619d5
AndyTheGameDev <and24903@gmail.com>
2021-07-11 18:31:24 +0530

    now I can check vowel lol

---
+++ Strings/CountVowel.java
@@ -0,0 +1,10 @@
+public class CountVowel {
+
+    public static boolean isVowel(char ch) {
+
+        ch = Character.toUpperCase(ch);
+        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || chat == 'U';
+
+    }
+
+}
