a4843d1f9e985b671682caf00046255eab248533
AndyTheGameDev <and24903@gmail.com>
2021-07-15 20:20:51 +0530

    idk

---
+++ Strings/BufferCapacity.java
@@ -0,0 +1,10 @@
+public class BufferCapacity {
+
+    public static void main(String[] args) {
+
+        StringBuffer str = new StringBuffer("s");
+        System.out.println(str.capacity());
+
+    }
+
+}
