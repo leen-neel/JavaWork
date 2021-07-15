2a38204ea2189efc57271357b9ad41041a7a0a3e
AndyTheGameDev <and24903@gmail.com>
2021-07-15 18:45:25 +0530

    idk

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LargestElement {
 
     public static int largestNumber(int[] arr) {
@@ -13,6 +17,27 @@ public class LargestElement {
         }
 
         return largest;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the length of the array : ");
+        int length = Integer.parseInt(in.readLine());
+
+        System.out.println();
+        int[] arr = new int[length];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the number for position " + i + " : ");
+            arr[i] = Integer.parseInt(in.readLine());
+        }
+
+        System.out.println();
+
+        System.out.println("The largest element of the array is : " + largestNumber(arr));
 
     }
 
