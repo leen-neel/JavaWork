6875cf379399d3468633b670aa6bc2f90abde6ec
AndyTheGameDev <and24903@gmail.com>
2021-07-18 18:49:26 +0530

    idk

---
+++ Strings/ReplaceFirst.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReplaceFirst {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a string : ");
+        String str = in.readLine();
+
+    }
+
+}
