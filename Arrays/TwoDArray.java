7d3c9694da3d0b1c6d5ef33f61d5464edf8694ce
AndyTheGameDev <and24903@gmail.com>
2021-07-31 18:27:00 +0530

    idk

--- Arrays/TwoDArray.java
+++ Arrays/TwoDArray.java
@@ -1,28 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TwoDArray {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         int length = 3;
-        int breadth =  3;
+        int breadth = 3;
 
         int[][] arr = new int[length][breadth];
 
         for (int i = 0; i < arr.length; i++) {
-            
+
             for (int j = 0; j < arr.length; j++) {
-                
-                System.out.print("Enter data for position ("i+", "+j+") : ");
-                
+
+                System.out.print("Enter data for position (" + i + ", " + j + ") : ");
+                arr[i][j] = Integer.parseInt(in.readLine());
 
             }
 
         }
 
+        System.out.println();
+
         for (int i = 0; i < arr.length; i++) {
-            
+
             for (int j = 0; j < arr.length; j++) {
-                
-                System.out.print(arr[i][j] "\\t");
+
+                System.out.print(arr[i][j] + "\t");
 
             }
 
