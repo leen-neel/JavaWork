aa62d291958592e901d06969d835cd94350d74d5
AndyTheGameDev <and24903@gmail.com>
2021-07-31 19:07:39 +0530

    idk

--- Arrays/LargestInRow.java
+++ Arrays/LargestInRow.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class LargestInRow {
@@ -31,6 +34,18 @@ public class LargestInRow {
         }
 
         return results;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the number of rows");
+        int numberOfRows = Integer.parseInt(in.readLine());
+
+        System.out.print("Enter the number of columns : ");
+        int numberOfColumns = Integer.parseInt(in.readLine());
 
     }
 
