39f2df3daa233f6bc1824f3d22e27832cf95c2ec
AndyTheGameDev <and24903@gmail.com>
2021-07-31 19:04:32 +0530

    idk

--- Arrays/LargestInRow.java
+++ Arrays/LargestInRow.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LargestInRow {
 
     public static void printArray(int[] arr) {
@@ -5,6 +7,30 @@ public class LargestInRow {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+
+    }
+
+    public static int[] rowLargest(int[][] arr, int numberOfRows) {
+
+        int max = 0;
+        int[] results = new int[numberOfRows];
+
+        for (int i = 0; i < numberOfRows; i++) {
+
+            for (int j = 0; j < arr[i].length; j++) {
+
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+
+            }
+
+            results[i] = max;
+            max = 0;
+
+        }
+
+        return results;
 
     }
 
