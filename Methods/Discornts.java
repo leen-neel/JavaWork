59fef840d2229dc529c410cc91086d594fb25f91
AndyTheGameDev <and24903@gmail.com>
2021-08-25 18:04:32 +0530

    idk

--- Methods/Discornts.java
+++ Methods/Discornts.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Discornts {
     
     int cost;
     String name;
     double dc;
     double amt;
+
+    public void input() throws IOException {
+        
+        BufferedReader in =  new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter name of the customer : ");
+        this.name = in.readLine();
+
+        System.out.print("Enter the price of the article : ");
+        this.cost = Integer.parseInt(in.readLine());
+        
+    }
 
     
 
