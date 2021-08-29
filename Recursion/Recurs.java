8f940211a2c03d984e4021965e5479b123f537eb
AndyTheGameDev <and24903@gmail.com>
2021-08-29 10:31:51 +0530

    idk

--- Recursion/Recurs.java
+++ Recursion/Recurs.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Recurs {
 
@@ -10,15 +12,21 @@ public class Recurs {
         
         if (count <= 5) {
             System.out.println(str);
+            recursion(str);
         }
-
-        recursion(str);
 
     }
 
     public static void main(String[] args) throws IOException {
         
-        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a string : ");
+        String str = in.readLine();
+
+        System.out.println();
+
+        recursion(str);
 
     }
 
