74ac0b424b57291aba1a6eab66920bff0c385f88
AndyTheGameDev <and24903@gmail.com>
2021-08-29 10:29:48 +0530

    idk

--- Recursion/Recurs.java
+++ Recursion/Recurs.java
@@ -2,6 +2,20 @@ import java.io.IOException;
 
 public class Recurs {
 
+    static int count = 0;
+
+    public static void recursion(String str) {
+        
+        count++;
+        
+        if (count <= 5) {
+            System.out.println(str);
+        }
+
+        recursion(str);
+
+    }
+
     public static void main(String[] args) throws IOException {
         
         
