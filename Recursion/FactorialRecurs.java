9ac138974e46a06610ce1ccf593cfc55dfbb7f3c
AndyTheGameDev <and24903@gmail.com>
2021-08-29 10:06:29 +0530

    idk

--- Recursion/FactorialRecurs.java
+++ Recursion/FactorialRecurs.java
@@ -1,19 +1,22 @@
 package Recursion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FactorialRecurs {
 
-    public static int fact(int n) {
+   
 
-        if (n == 1) {
-            return 1;
-        } else {
-            return (n * fact(n - 1));
-        }
+    public static void main(String[] args) throws IOException {
 
-    }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        System.out.println(fact(5));
+        System.out.print("Enter a number : ");
+        int n = Integer.parseInt(in.readLine());
+
+        
+
     }
 
 }
