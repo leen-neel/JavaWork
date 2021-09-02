44844fd28826971427b7f244583578be6145091e
AndyTheGameDev <and24903@gmail.com>
2021-09-02 08:44:18 +0530

    idk

---
+++ Recursion/HCF.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HCF {
+
+    public static int hcf(int a, int b) {
+        
+        if (b == 0) {
+            return a;
+        }
+        return hcf(b, a % b);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter a number : ");
+        int a = Integer.parseInt(in.readLine());
+
+        System.out.print("Enter another number : ");
+        int b = Integer.parseInt(in.readLine());
+
+        System.out.println("The HCF of " + a + " and " + b + " is : " + hcf(a, b));
+    }
+
+}
