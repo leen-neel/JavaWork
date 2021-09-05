a289fac8fcb8ab0de6e9ecd8f56e6eb10029e2a4
AndyTheGameDev <and24903@gmail.com>
2021-09-05 11:00:07 +0530

    idk

--- Recursion/ReverseNumber.java
+++ Recursion/ReverseNumber.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 
 public class ReverseNumber {
@@ -18,6 +21,31 @@ public class ReverseNumber {
 
     }
 
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
 
+        while (true) {
+            
+            
+            try {
+                
+                System.out.print("Enter a number : ");
+                n = Integer.parseInt(in.readLine());
+                System.out.print("The number reversed is ");
+                revNum(n);
+                break;
+
+            } catch (NumberFormatException ex) {
+
+                System.err.println("Please enter a number instead of a string");
+                System.out.println();
+
+            }
+
+        }
+
+    }
 
 }
