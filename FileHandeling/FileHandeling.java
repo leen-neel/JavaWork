9b6bc19dd6f0ddb5be3ff68c48d80b933bd7b189
leen-neel <and24903@gmail.com>
2021-10-21 17:57:52 +0530

    idk

--- FileHandeling/FileHandeling.java
+++ FileHandeling/FileHandeling.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 
 public class FileHandeling {
 
-    public static String removeSpaces(String str) {
-        String spaceLess = "";
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'T') {
-                spaceLess += '#';
-            } else {
-                spaceLess += str.charAt(i);
-            }
-        }
-
-        return spaceLess;
-    }
-
     public static void main(String[] args) throws IOException {
 
         File file = new File("/home/and249/RandomJavaStuff/Help_Pls/FileHandeling/text.txt");
@@ -28,7 +14,7 @@ public class FileHandeling {
         String str = sc.next();
         sc.close();
 
-        System.out.println(removeSpaces(str));
+        System.out.println(str);
 
     }
 
