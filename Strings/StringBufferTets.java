0a2645861cebf4a1f3c33148d0d6e0e0add47226
leen-neel <and24903@gmail.com>
2022-03-25 09:21:26 +0530

    idk

---
+++ Strings/StringBufferTets.java
@@ -0,0 +1,12 @@
+public class StringBufferTets {
+
+    public static void main(String[] args) {
+        
+        String str = "test";
+        StringBuffer strb = new StringBuffer(str);
+
+        System.out.println(strb);
+
+    }
+    
+}
