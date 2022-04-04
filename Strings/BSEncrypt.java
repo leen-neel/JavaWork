e058d7d9ebdfbff7d1d1f4def753720f9c9f9829
leen-neel <and24903@gmail.com>
2022-04-04 20:41:47 +0530

    gosh ❤❤❤

--- Strings/BSEncrypt.java
+++ Strings/BSEncrypt.java
@@ -47,11 +47,70 @@ public class BSEncrypt {
         
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Enter a string : ");
-        String str = in.readLine();
+        String str = "";
+        int choice = 0;
+        int tryAgain = 0;
 
-        
-        System.out.println(decrypt(encrypt(str)));
+        while (true) {
+            
+            // Present user with options
+            System.out.println("Choose an option : ");
+            System.out.println("1. Encrypt");
+            System.out.println("2. Decrypt");
+    
+            try {
+                
+                System.out.print("Enter your choice : ");
+                choice = Integer.parseInt(in.readLine());
+    
+                System.out.println();
+    
+                switch (choice) {
+                    case 1:
+                        
+                        System.out.print("Enter a string : ");
+                        str = in.readLine();
+                        System.out.println(encrypt(str));
+    
+                    break;
+    
+                    case 2:
+    
+                        System.out.print("Enter a string : ");
+                        str = in.readLine();
+                        System.out.println(decrypt(str));
+    
+                    break;
+                
+                    default:
+                        System.out.println("Invalid choice");
+                    break;
+                }
+
+                System.out.println();
+
+                // Ask user if they would like to try again
+                System.out.println("Would you like to try again?");
+                System.out.println("Type 1 to try again and 2 to exit");
+                System.out.print("Enter your choice : ");
+                tryAgain = Integer.parseInt(in.readLine());
+
+                System.out.println();
+
+                if (tryAgain == 1) {
+                    continue;
+                } else if (tryAgain == 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid number. Assuming you want to try again...");
+                    continue;
+                }
+                
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid number :)\n"); // Catch exception where the user hasn't entered a number when they're supposed to
+            }            
+
+        }
 
     }
     
