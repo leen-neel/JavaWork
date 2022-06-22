1642bbf968fa7cfacc13e5a904c6fcd461ceaa2b
leen-neel <and24903@gmail.com>
2022-06-22 18:59:47 +0530

    interfaces are kinda depressing ngl

--- Interfaces/TestIG.java
+++ Interfaces/TestIG.java
@@ -3,22 +3,27 @@
  */
 interface Animal {
 
-    
-
     public void animalSound();
     public void sleep();
     
 }
 
 class Pig implements Animal {
+
+    public String name;
+
+    Pig(String name) {
+        this.name = name;
+    }
+
     @Override
     public void animalSound() {
-        System.out.println("Oink oink");
+        System.out.println(this.name + " says: Oink oink");
     }
 
     @Override
     public void sleep() {
-        System.out.println("*sleeps in oink oink*");
+        System.out.println(this.name + " is sleeping. Shhhhh! 🤫");
     }
 }
 
@@ -28,7 +33,7 @@ class Pig implements Animal {
 public class TestIG {
 
     public static void main(String[] args) {
-        Pig steven = new Pig();
+        Pig steven = new Pig("Steven");
 
         steven.animalSound();
         steven.sleep();
