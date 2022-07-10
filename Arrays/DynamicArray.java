a11e030595db11959aeb517c507df92e08d6ee71
leen-neel <and24903@gmail.com>
2022-07-10 19:32:11 +0530

    ugg

--- Arrays/DynamicArray.java
+++ Arrays/DynamicArray.java
@@ -1,12 +1,12 @@
 /**
  * DynamicArray
  */
-public class Array {
+public class DynamicArray {
 
     private int arr[];
     private int length;
 
-    public Array(int length) {
+    public DynamicArray(int length) {
         this.arr = new int[length];
         this.length = length;
     }
@@ -21,7 +21,32 @@ public class Array {
                 newArr[i] = arr[i];
             }
 
+            arr = newArr;
+
         }
+
+        arr[this.length++] = element;
+
+    }
+
+    public void printArray() {
+        
+        for (int i = 0; i < this.length; i++) {
+            System.err.print(this.arr[i] + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        DynamicArray arr = new DynamicArray(3);
+
+        arr.insert(1);
+        arr.insert(2);
+        arr.insert(3);
+        //arr.insert(4);
+
+        arr.printArray();
 
     }
     
