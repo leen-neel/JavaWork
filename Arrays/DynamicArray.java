e5372228808a4860da90064b31a4f780dc15eeab
leen-neel <and24903@gmail.com>
2022-07-10 18:57:07 +0530

    trying shit out idk

---
+++ Arrays/DynamicArray.java
@@ -0,0 +1,16 @@
+/**
+ * DynamicArray
+ */
+public class Array {
+
+    private int arr[];
+    private int length;
+
+    public Array(int length) {
+        this.arr = new int[length];
+        this.length = length;
+    }
+
+
+    
+}
