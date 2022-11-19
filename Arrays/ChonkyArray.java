2cbe9060a5916b17df20fbb5f4aad5644e2af067
leen-neel <and24903@gmail.com>
2022-11-19 11:51:29 +0530

    I hate my old code

--- Arrays/ChonkyArray.java
+++ Arrays/ChonkyArray.java
@@ -8,7 +8,8 @@ public class ChonkyArray {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int[][] arr = new int[5][5];
+        int[][] arr = new int[2][2];
+        int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -16,24 +17,13 @@ public class ChonkyArray {
 
                 System.out.print("Enter data for position (" + i + ", " + j + ") : ");
                 arr[i][j] = Integer.parseInt(in.readLine());
-
-            }
-
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            for (int j = 0; j < arr.length; j++) {
-
                 sum += arr[i][j];
 
             }
 
         }
 
-        System.out.println("Sum is " + sum);
+        System.out.println("\nSum is " + sum);
 
     }
 
