101c5557484c9faf89853c9195d5a4fbffbfd526
leen-neel <and24903@gmail.com>
2022-11-19 11:23:01 +0530

    kinda works ig? idk lol and idc

--- Arrays/DynamicArray.java
+++ Arrays/DynamicArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * DynamicArray
  */
@@ -12,41 +14,69 @@ public class DynamicArray {
     }
 
     public void insert(int element) {
-        
-        if (this.arr.length == this.length) {
-            
-            int newArr[] = new int[2 * this.length];
 
-            for (int i = 0; i < this.length; i++) {
+        if (this.arr.length == this.length) {
+
+            int[] newArr = new int[this.length + 1];
+
+            // Adding the elements of the old array to the new array
+            for (int i = 0; i < arr.length; i++) {
                 newArr[i] = arr[i];
             }
 
-            arr = newArr;
-
+            newArr[newArr.length - 1] = element; // Add the given element to the end of the new array
+            this.length = newArr.length; // Update the length
+            
+            // Replace the array with the new array
+            this.arr = new int[this.length];
+            this.arr = newArr;
+            
         }
 
-        arr[this.length++] = element;
-
+        // arr[this.length++] = element;
+        
     }
 
-    public void printArray() {
+    // public void insert(int element) {
         
+    //     if (this.arr.length == this.length) {
+            
+    //         int newArr[] = new int[2 * this.length];
+
+    //         for (int i = 0; i < this.length; i++) {
+    //             newArr[i] = arr[i];
+    //         }
+
+    //         arr = newArr;
+
+    //     }
+
+    //     arr[this.length++] = element;
+
+    // }
+
+    public String toString() {
+        
+        String str = "";
+
         for (int i = 0; i < this.length; i++) {
-            System.err.print(this.arr[i] + " ");
+            str += this.arr[i] + " ";
         }
+
+        return str.trim();
 
     }
 
     public static void main(String[] args) {
         
-        DynamicArray arr = new DynamicArray(3);
+        DynamicArray arr = new DynamicArray(0);
 
         arr.insert(1);
         arr.insert(2);
         arr.insert(3);
-        //arr.insert(4);
+        arr.insert(4);
 
-        arr.printArray();
+        System.out.println(arr);
 
     }
     
