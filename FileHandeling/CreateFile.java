8bc3fcb48620e5bf0559f561e771b4582606e045
leen-neel <and24903@gmail.com>
2022-11-20 23:49:11 +0530

    kek

--- FileHandeling/CreateFile.java
+++ FileHandeling/CreateFile.java
@@ -6,11 +6,15 @@ public class CreateFile {
 
     public static void main(String[] args) throws IOException {
         
-        File fl = new File("./yes.txt");
+        File fl = new File("./no.txt");
     
         if (fl.createNewFile()) {
 
             System.out.println("File " + fl.getName() + " created.");
+
+            FileWriter flWriter = new FileWriter(fl.getAbsoluteFile());
+            flWriter.write("yes wassup");
+            flWriter.close();
 
         } else {
             
